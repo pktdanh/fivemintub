1e2c9a18347b1c4139584fdb10a3aa12bd3e53fe
pktdanh <pktdanh@gmail.com>
2022-06-02 23:03:40 +0700

    update ...

--- trydjango4/settings.py
+++ trydjango4/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'pb3%dunhdf2t4v+bmmnvt^dz1kpp5d$sr59c50x=9rf@$#@g$('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG = True
 
 ALLOWED_HOSTS = ['fourpktdapp.herokuapp.com']
 
